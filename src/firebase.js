6afe31a88b3092e3660116e836d5b26d68a3a366
Sea Lion <aungchitmin1112000@gmail.com>
2022-12-22 08:31:38 +0630

    initializing google keep clone

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnG90XuQtjxBQOKQuHbRdGWAtV7zVR3Vc",
+  authDomain: "keep-clone-5f83a.firebaseapp.com",
+  projectId: "keep-clone-5f83a",
+  storageBucket: "keep-clone-5f83a.appspot.com",
+  messagingSenderId: "805513027777",
+  appId: "1:805513027777:web:ff11ce00ec937a83b27440"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
